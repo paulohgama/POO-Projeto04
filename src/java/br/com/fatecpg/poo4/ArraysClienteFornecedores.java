b0b541f6d00d0b6adee83e69e352e6630e970bfd
acbertini <acbertini@gmail.com>
2018-04-20 21:55:24 -0300

    Array e CRUD Fornecedores

--- src/java/br/com/fatecpg/poo4/ArraysClienteFornecedores.java
+++ src/java/br/com/fatecpg/poo4/ArraysClienteFornecedores.java
@@ -8,11 +8,17 @@ import java.util.ArrayList;
  */
 public class ArraysClienteFornecedores {
     public static ArrayList<Clientes> clienteslist = new ArrayList();
+    public static ArrayList<Fornecedores> fornecedoresList = new ArrayList();
+    
     public static String nome = "add";
     public static String valor = "Adicionar";
     public static String requerido = "required";
 
     public static ArrayList<Clientes> getClienteslist() {
         return clienteslist;
+    }
+    
+    public static ArrayList<Fornecedores> getFornecedoreslist() {
+        return fornecedoresList;
     }
 }
---
+++ src/java/br/com/fatecpg/poo4/Fornecedores.java
@@ -0,0 +1,65 @@
+package br.com.fatecpg.poo4;
+
+/**
+ *
+ * @author AnaBertini
+ */
+public class Fornecedores {
+    private String nome;
+    private String razaoSocial;
+    private String cnpj;
+    private String email;
+    private String telefone;
+    private String endereco;
+    public static int controle;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+}
